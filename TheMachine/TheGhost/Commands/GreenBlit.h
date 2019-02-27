2aca3d725f46f46a02d0bcf34f1d72615e6123f6
TODDM\Todd <toddmoulder88@gmail.com>
2019-02-27 13:26:38 -0600

    Command update.  Git is confusing :/

--- TheMachine/TheGhost/Commands/GreenBlit.h
+++ TheMachine/TheGhost/Commands/GreenBlit.h
@@ -4,7 +4,7 @@
 //    This will remain until the system is reset.
 //    This routine should continute to run, at x time units, blit another pixel
 //    Max pixels blited ~= Screen.Width / 100
-class GreenBlit : ICommand
+class GreenBlit : public ICommand
 {
 public:
 	void Execute(std::queue<std::string>* args) override;
